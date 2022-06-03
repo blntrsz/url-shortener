075bea9138003a8e062302c77f994b00e371dfab
Balint Orosz <orosz.balint.97@gmail.com>
2022-06-03 23:19:55 +0200

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import { withTRPC } from "@trpc/next";
+import { AppType } from "next/dist/shared/lib/utils";
+import { AppRouter } from "./api/trpc/[trpc]";
+import "../styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+const getBaseUrl = () => {
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+};
+
+export default withTRPC<AppRouter>({
+  config: ({ ctx }) => ({
+    url: `${getBaseUrl()}/api/trpc`,
+  }),
+  ssr: false,
+})(MyApp);
